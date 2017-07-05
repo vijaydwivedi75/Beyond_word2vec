fc422f0e7c7e7eb984fd5bd9ecb8a897e74443c9
Vijay Prakash Dwivedi <vijaydwivedi75@gmail.com>
2017-07-06 01:24:12 +0530

    added evals

--- evals.py
+++ evals.py
@@ -105,26 +105,60 @@ def eval_analogies(a_word, b_word, c_word, num_analogies):
 	dist = tf.matmul(target, norm_word_embedding, transpose_b=True)
 	tf.InteractiveSession()
 	_, pred_idx = tf.nn.top_k(dist.eval(), 4)
+	pred_ids = pred_idx.eval()
 
+	# print(pred_ids)
 	for i in range(num_analogies):
-		print("* " + str(id_word[a_word[i]]) + " : " + str(id_word[b_word[i]]) + "  =>  "
-			+ str(id_word[c_word[i]]) + " :::: " + str(id_word[pred_idx[i][0]]) + str(id_word[pred_idx[i][1]])
-			+ str(id_word[pred_idx[i][2]]) + str(id_word[pred_idx[i][3]]))
+		print("* IF '" + str(id_word[a_word[i]]) + "'' is to '" + str(id_word[b_word[i]]) + "'  THEN  '"
+			+ str(id_word[c_word[i]]) + "'' :: " + str(id_word[pred_ids[i][0]]) + ", " + str(id_word[pred_ids[i][1]])
+			+ ", " + str(id_word[pred_ids[i][2]]) + ", " + str(id_word[pred_ids[i][3]]))
 
 def main():
 	# 1. EVALUATION FOR NEARBY WORDS
 	# eval_nearby(1540, 1565)
 
 	# 2. EVALUATION FOR WORD ANALOGIES
-	num_analogies = 15
+	num_analogies = 10
 	analogy_a = []
 	analogy_b = []
 	analogy_c = []
-	for i in range(num_analogies):
-		analogy_a.append(randint(0, len(word_embd)))
-		analogy_b.append(randint(0, len(word_embd)))
-		analogy_c.append(randint(0, len(word_embd)))
+	# for i in range(num_analogies):
+	# 	analogy_a.append(randint(0, len(word_embd)))
+	# 	analogy_b.append(randint(0, len(word_embd)))
+	# 	analogy_c.append(randint(0, len(word_embd)))
 
+	analogy_a.append(word_id['france'])
+	analogy_a.append(word_id['big'])
+	analogy_a.append(word_id['sarkozy'])
+	analogy_a.append(word_id['dark'])
+	analogy_a.append(word_id['king'])
+	analogy_a.append(word_id['is'])
+	analogy_a.append(word_id['apple'])
+	analogy_a.append(word_id['brother'])
+	analogy_a.append(word_id['chicago'])
+	analogy_a.append(word_id['mouse'])
+
+	analogy_b.append(word_id['paris'])
+	analogy_b.append(word_id['bigger'])
+	analogy_b.append(word_id['france'])
+	analogy_b.append(word_id['darker'])
+	analogy_b.append(word_id['queen'])
+	analogy_b.append(word_id['was'])
+	analogy_b.append(word_id['fruit'])
+	analogy_b.append(word_id['sister'])
+	analogy_b.append(word_id['usa'])
+	analogy_b.append(word_id['mice'])
+
+	analogy_c.append(word_id['italy'])
+	analogy_c.append(word_id['small'])
+	analogy_c.append(word_id['merkel'])
+	analogy_c.append(word_id['soft'])
+	analogy_c.append(word_id['man'])
+	analogy_c.append(word_id['have'])
+	analogy_c.append(word_id['coffee'])
+	analogy_c.append(word_id['uncle'])
+	analogy_c.append(word_id['athens'])
+	analogy_c.append(word_id['dollar'])
 	eval_analogies(analogy_a, analogy_b, analogy_c, num_analogies)
 
 main()
